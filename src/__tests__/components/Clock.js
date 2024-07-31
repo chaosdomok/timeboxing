989381ab8c991d619cc5f905ff78d5e1f95a4a6d
Dominik <dominikfurgacz2703@gmail.com>
2024-07-31 13:09:19 +0200

    Test Clock component witch reactTestRender and snapshosts

--- src/__tests__/components/Clock.js
+++ src/__tests__/components/Clock.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Clock from "../../components/TimeboxComponents/Clock";
 import ReactDOM from "react-dom";
+import renderer from 'react-test-renderer'
 
 var root = null;
+var clockRenderer = null;
 
 describe('<Clock />', () => {
     describe('when given minutes and seconds', () => {
@@ -30,6 +32,28 @@ describe('<Clock />', () => {
         });
     });
 
+    describe('when given minutes and seconds (TestRenderer)', () => {
+        beforeEach(() => {
+            clockRenderer = renderer.create(
+                <Clock minutes={10} seconds={20} />
+            )
+        });
+        it('renders properly', () => {
+            console.log(clockRenderer.toJSON())
+            expect(clockRenderer.toJSON()).toMatchSnapshot();  
+        });
+        it('renders an h2 element', () => {
+            expect(clockRenderer.toJSON().type).toEqual("h2")  
+        });
+    
+        it('renders Clock element', () => {
+            expect(clockRenderer.toJSON().props).toMatchObject({"className": expect.stringMatching(/Clock/)
+        });
+    
+        it('renders time properly', () => {
+            expect(clockRenderer.toJSON().children).toEqual(expect.arrayContaining(["10", "20"]))
+        });
+    });
 
     it('sets className to empty string if not given anything else', () => {
         expect(<Clock minutes={10} seconds={20} />).toEqual(<Clock className="" minutes={10} seconds={20} />)
